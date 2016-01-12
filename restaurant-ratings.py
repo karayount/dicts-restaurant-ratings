5c7bf65e80cfe159b6cb51b0e416dd4ab7faa76a
Hackbright Student <info@hackbrightacademy.com>
2016-01-11 16:30:41 -0800

    wrapped read and create dictionary in function

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,18 +1,21 @@
 restaurant_rating_file = "scores.txt"
 
-restaurant_ratings = {}
+def read_ratings_file(restaurant_rating_file):
+    restaurant_ratings = {}
+    with open(restaurant_rating_file) as restaurant_rating_text:
+        for line in restaurant_rating_text:
+            rating_tokens = line.rstrip().split(":")
+            # check to see whether entry is already in dictionary
+            # add restaurant to dictionary as key, and assign rating to value (perhaps make value a number, not string)
+            restaurant_ratings[rating_tokens[0]] = int(rating_tokens[1])
+    return restaurant_ratings
 
-with open(restaurant_rating_file) as restaurant_rating_text:
-    for line in restaurant_rating_text:
-        rating_tokens = line.rstrip().split(":")
-        # check to see whether entry is already in dictionary
-        # add restaurant to dictionary as key, and assign rating to value (perhaps make value a number, not string)
-        restaurant_ratings[rating_tokens[0]] = int(rating_tokens[1])
 
+restaurant_ratings = read_ratings_file(restaurant_rating_file)
 print restaurant_ratings
-#
 # A
 # assign sorted list of keys to a variable
+
 # loop through list printing that key in dictionary with its value
 # 
 # B
