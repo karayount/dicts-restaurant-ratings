913e90cdd523efa4cb50086a74fb2ee2b0bf9c2a
Hackbright Student <info@hackbrightacademy.com>
2016-01-11 16:49:25 -0800

    Docstrings added for functions, ratings reading and printing working.

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,28 +1,31 @@
 restaurant_rating_file = "scores.txt"
 
 def read_ratings_file(restaurant_rating_file):
+    """Creates dictionary of restaurant name keys to rating values from file"""
+
     restaurant_ratings = {}
     with open(restaurant_rating_file) as restaurant_rating_text:
         for line in restaurant_rating_text:
             rating_tokens = line.rstrip().split(":")
-            # check to see whether entry is already in dictionary
-            # add restaurant to dictionary as key, and assign rating to value (perhaps make value a number, not string)
             restaurant_ratings[rating_tokens[0]] = int(rating_tokens[1])
     return restaurant_ratings
 
-restaurant_ratings = read_ratings_file(restaurant_rating_file)
-# print restaurant_ratings
 
 def print_ratings(ratings):    
-    # A
-    # assign sorted list of keys to a variable
-    restaurant_names = sorted(ratings.keys())
-    # loop through list printing that key in dictionary with its value
-    for restaurant in restaurant_names:
-        print restaurant, "is rated at", ratings[restaurant], "."
+    """Prints alphabetized restaurant names with their rating from input dictionary"""
 
+    restaurant_names = sorted(ratings.keys())
+    for restaurant in restaurant_names:
+        print "%s is rated at %d." % (restaurant, ratings[restaurant])
+
+
+restaurant_ratings = read_ratings_file(restaurant_rating_file)
 print_ratings(restaurant_ratings)
-# B
+
+
+
+
+# print method B
 # get tuples of items in dictionary
 # sort that list of tuples
 # loop through, printing items
