97aba5329d46805cd936326929bc81d20c807b6a
Hackbright Student <info@hackbrightacademy.com>
2016-01-11 16:27:07 -0800

    Working dict creation and fill

---
+++ restaurant-ratings.py
@@ -0,0 +1,23 @@
+restaurant_rating_file = "scores.txt"
+
+restaurant_ratings = {}
+
+with open(restaurant_rating_file) as restaurant_rating_text:
+    for line in restaurant_rating_text:
+        rating_tokens = line.rstrip().split(":")
+        # check to see whether entry is already in dictionary
+        # add restaurant to dictionary as key, and assign rating to value (perhaps make value a number, not string)
+        restaurant_ratings[rating_tokens[0]] = int(rating_tokens[1])
+
+print restaurant_ratings
+#
+# A
+# assign sorted list of keys to a variable
+# loop through list printing that key in dictionary with its value
+# 
+# B
+# get tuples of items in dictionary
+# sort that list of tuples
+# loop through, printing items
+
+
